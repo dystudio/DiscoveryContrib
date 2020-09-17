4932bad31cced227c6ccc31d8737e2ffb6601b9a
HaojunRen <1394997@qq.com>
2020-09-17 08:33:50 +0800

    优化代码

--- discovery-contrib-database/discovery-contrib-database-starter-shardingsphere/src/main/java/com/nepxion/discovery/contrib/database/shardingsphere/constant/ShardingSphereContribConstant.java
+++ discovery-contrib-database/discovery-contrib-database-starter-shardingsphere/src/main/java/com/nepxion/discovery/contrib/database/shardingsphere/constant/ShardingSphereContribConstant.java
@@ -1,5 +1,14 @@
 package com.nepxion.discovery.contrib.database.shardingsphere.constant;
 
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
 import com.nepxion.discovery.contrib.core.constant.ContribConstant;
 
 public class ShardingSphereContribConstant extends ContribConstant {
